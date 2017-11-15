ebc02a01a8d9186d6e75b6f8e18b9dce802bf191
whiz-kid <ac9184511@gmail.com>
2017-11-15 10:59:48 +0530

    xgboost model

--- model.py
+++ model.py
@@ -6,6 +6,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
+from xgboost import XGBClassifier
+
 
 train=pd.read_csv("train.csv")
 test=pd.read_csv("test.csv")
@@ -17,7 +19,7 @@ cont_cols=["cont_1","cont_2","cont_3","cont_4","cont_5","cont_6","cont_7","cont_
 cat_cols=["cat_1","cat_2","cat_3","cat_4","cat_5","cat_6","cat_7","cat_8","cat_9","cat_10","cat_11",\
 			"cat_12","cat_13","cat_14","cat_15","cat_16","cat_17","cat_18","cat_19","cat_20","cat_21",\
 			"cat_22","cat_23"]
-new_cols=['cont_2','cat_1','cat_2','cat_20','cat_21','cat_22','cat_23']
+new_cols=['cont_2','cat_1','cat_2','cat_20','cat_21','cat_22']
 
 # print("taking train and test data")
 # y_train=train["target"]
@@ -43,20 +45,19 @@ new_cols=['cont_2','cat_1','cat_2','cat_20','cat_21','cat_22','cat_23']
 
 
 x_train=train.loc[:,new_cols]
-x_train['cat_20_21']=x_train['cat_20']+x_train['cat_21']
-x_train.drop(['cat_20','cat_21'],axis=1)
-x_train['cat_1_20_21']=x_train['cat_1']+x_train['cat_20_21']
-x_train.drop(['cat_1','cat_20_21'],axis=1)
+x_train['cat_1_20_21']=x_train['cat_1']+x_train['cat_20']+x_train['cat_21']
+x_train=x_train.drop(['cat_1','cat_20','cat_21'],axis=1)
 y_train=np.ravel(train["target"])
 
 x_test=test.loc[:,new_cols]
-x_test['cat_20_21']=x_test['cat_20']+x_test['cat_21']
-x_test.drop(['cat_20','cat_21'],axis=1)
-x_test['cat_1_20_21']=x_test['cat_1']+x_test['cat_20_21']
-x_test.drop(['cat_1','cat_20_21'],axis=1)
+x_test['cat_1_20_21']=x_test['cat_1']+x_test['cat_20']+x_test['cat_21']
+x_test=x_test.drop(['cat_1','cat_20','cat_21'],axis=1)
 
 
+
+# print(x_test.head())
 clf=RandomForestClassifier(criterion= 'entropy', max_depth=10)
+# clf=XGBClassifier(max_depth=10)
 eq=clf.fit(x_train,y_train)
 pred=eq.predict(x_test)
 pred=np.ravel(pred)
@@ -68,4 +69,4 @@ pred=np.ravel(pred)
 sub = pd.read_csv('sample_submission.csv')
 sub['target'] = pred
 sub['target'] = sub['target'].astype(int)
-sub.to_csv('sub4.csv', index=False)
+sub.to_csv('sub_day2_1.csv', index=False)
