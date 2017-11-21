4ff8bd5947c543e84d19c72768f4e3e2f2523758
whiz-kid <ac9184511@gmail.com>
2017-11-21 11:02:40 +0530

    xgboost tuned

--- model_2.py
+++ model_2.py
@@ -6,44 +6,11 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
-from xgboost import XGBClassifier
-from sklearn.model_selection import train_test_split
+import xgboost as xgb
 
 train=pd.read_csv("train.csv")
 test=pd.read_csv("test.csv")
-
-remove_cols=["connection_id","target"]
-cont_cols=["cont_1","cont_2","cont_3","cont_4","cont_5","cont_6","cont_7","cont_8","cont_9",\
-			"cont_10","cont_11","cont_12","cont_13","cont_14","cont_15","cont_16","cont_17",\
-			"cont_18"]
-cat_cols=["cat_1","cat_2","cat_3","cat_4","cat_5","cat_6","cat_7","cat_8","cat_9","cat_10","cat_11",\
-			"cat_12","cat_13","cat_14","cat_15","cat_16","cat_17","cat_18","cat_19","cat_20","cat_21",\
-			"cat_22","cat_23"]
 new_cols=['cont_2','cont_3','cont_13','cont_14','cat_2','cat_9','cat_20','cat_21','cat_22']
-
-# print("taking train and test data")
-# y_train=train["target"]
-# x_cont_train=train.loc[:,cont_cols]
-# x_cat_train=train.loc[:,cat_cols]
-
-# print("training continuous model")
-# clf_cont=RandomForestClassifier(max_depth=10)
-# eq_cont=clf_cont.fit(x_cont_train,y_train)
-
-# print("training categorical model")
-# clf_cat=RandomForestClassifier(max_depth=10)
-# eq_cat=clf_cat.fit(x_cat_train,y_train)
-
-# x_train=train.drop(remove_cols,axis=1)
-# x_test=test.drop(["connection_id"],axis=1)
-# y_train=train["target"]
-
-# print("print class score")
-# print(eq_cat.score(x_cat_train,y_train))
-# print(eq_cont.score(x_cont_train,y_train))
-# print(eq.feature_importances_)
-
-
 x_train=train.loc[:,new_cols]
 x_train['cat_20_21']=x_train['cat_20']+x_train['cat_21']
 x_train=x_train.drop(['cat_20','cat_21'],axis=1)
@@ -52,36 +19,45 @@ y_train=np.ravel(train["target"])
 x_test=test.loc[:,new_cols]
 x_test['cat_20_21']=x_test['cat_20']+x_test['cat_21']
 x_test=x_test.drop(['cat_20','cat_21'],axis=1)
-# x_test['cat_1_20_21']=x_test['cat_1']+x_test['cat_20']+x_test['cat_21']
-# x_test=x_test.drop(['cat_1','cat_20','cat_21'],axis=1)
+params={
+    "objective":"multi:softmax",     
+    "learning_rate":0.1,
+    "subsample":0.8,
+    "colsample_bytree": 0.8,
+#     'eval_metric':'auc',
+    "max_depth":6,
+    'silent':1,
+    'nthread':3,
+    'num_class':3
+        
+       } 
 
+x_train=np.array(x_train)
+y_train=np.array(y_train)
+x_test=np.array(x_test)
 
+kf=KFold(n=len(x_train),n_folds=10,random_state=100)
+scores=[]
+predictions=[]
+for trainkf,testkf in kf:
+    train_data_x,train_data_y=x_train[trainkf],y_train[trainkf]
+    valid_data_x,valid_data_y=x_train[testkf],y_train[testkf]
+    
+    dtrain=xgb.DMatrix(train_data_x,train_data_y)
+    dvalid=xgb.DMatrix(valid_data_x,valid_data_y)
+    dtest=xgb.DMatrix(x_test)
+    watchlist=[(dtrain,'train'),(dvalid,'valid')]
+    gbm=xgb.train(params,dtrain,6000,evals=watchlist,early_stopping_rounds=50,verbose_eval=1)
+    cv=gbm.predict(dvalid)
+    lst=gbm.predict(dtest)
+    scores.extend(cv)
+    predictions.append(lst)
 
-# print(x_test.head())
-clf=RandomForestClassifier(criterion= 'entropy', max_depth=10)
-# clf=XGBClassifier(max_depth=10)
-x,xx,y,yy=train_test_split(x_train,y_train,test_size=0.2,random_state=10)
-eq=clf.fit(x,y)
-# pred=eq.predict(x_test)
-# pred=np.ravel(pred)
-print(eq.score(xx,yy))
-
-# 22,24,30,33,35
-# cat4,cat6,cat12,cat15,cat17
-
-# sub = pd.read_csv('sample_submission.csv')
-# sub['target'] = pred
-# sub['target'] = sub['target'].astype(int)
-# sub.to_csv('sub_day3_2.csv', index=False)
-
-
-
-# clf=RandomForestClassifier(max_depth=10,criterion='entropy')
-# columns=[x for x in train.columns if x not in ['connection_id','target']]
-# target=np.ravel(train['target'])
-
-# for column in columns:
-# 	clf.fit(train[column].values.reshape(-1,1),target)
-# 	score=clf.score(train[column].values.reshape(-1,1),target)
-# 	if(score>=0.70):
-# 		print("{}".format(column))
+from scipy.stats import mode
+pred=mode(predictions,axis=0)
+pred=pred[0][0]
+pred=np.ravel(pred)
+sub = pd.read_csv('sample_submission.csv')
+sub['target'] = pred
+sub['target'] = sub['target'].astype(int)
+sub.to_csv('sub_day4_1.csv', index=False)
